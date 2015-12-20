49b0347b57d0b02e3b6c2587c4201cd549dcc317
Roby Poteau <robopoto@gmail.com>
2015-12-20 09:36:57 -0800

    ...

--- scripts/input_bistable.sh
+++ scripts/input_bistable.sh
@@ -1,2 +1,2 @@
 #~/bin/bash
-./bin/prog -s bistable_switch -t 0:.001:1 -i 0:.001:1 -u 150,1,2 -o 125,1.9,1.3 -y 24,23 -n 0.05 > results/std.txt
+./bin/prog -s bistable_switch -t 0:.001:1 -i 0:.001:1 -u 150,1,2 -o 125,1.9,1.3 -y 24,23 -k 25 -n 0.05 > results/std.txt
